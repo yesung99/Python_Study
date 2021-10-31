a56b40b3f283d596edf52cddcbd586c6ce10d97c
이예성 <89582059+yesung99@users.noreply.github.com>
2021-10-31 20:26:36 +0900

    Create 234. Palindrome Linked List.py

---
+++ 234. Palindrome Linked List.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        q: List = []
+
+        if not head: # 헤드에 아무것도 없으면 팰린드롬
+            return True
+
+        node = head
+        # 리스트 변환
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        # 팰린드롬 판별
+        while len(q) > 1:
+            if q.pop(0) != q.pop():
+                return False
+
+        return True
